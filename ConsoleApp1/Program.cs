d62a003b274409ad14951fd939df1f3ad2efb52c
Developer <developer>
2022-09-02 11:21:44 +0530

    TEST 2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,6 +8,7 @@ namespace ConsoleApp1
         {
             Console.WriteLine("HI ASDFASDF!");
             Console.WriteLine("HI ASDFASDF!");
+            Console.WriteLine("HI ASDFASDF!");
         }
     }
 }
