af4bcc8e40f0d09d642d7f2b88ecd31e98c28af2
Developer <developer>
2022-09-02 11:21:32 +0530

    TEST

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("HI!");
+            Console.WriteLine("HI ASDFASDF!");
+            Console.WriteLine("HI ASDFASDF!");
         }
     }
 }
